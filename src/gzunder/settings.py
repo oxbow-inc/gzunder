2c99b96e4e3d9c60c8403ab3f9e053d682a35469
lainiwa <kirrik96@gmail.com>
2021-05-21 02:50:33 +0300

    Add postgres host fqdn validation

--- src/gzunder/settings.py
+++ src/gzunder/settings.py
@@ -1,4 +1,5 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, PostgresDsn, validator, conint
+from fqdn import FQDN
 
 
 class Settings(BaseSettings):
@@ -6,7 +7,15 @@ class Settings(BaseSettings):
     pg_pass: str
     pg_db: str
     pg_host: str
-    pg_port: int
+    pg_port: conint(ge=0, le=65535)
+
+    # TODO add `pg_url: PostgresDsn` computed field
+    # when https://github.com/samuelcolvin/pydantic/pull/2625 ready
+
+    @validator("pg_host")
+    def pg_host_is_fqdn(cls, v):
+        assert FQDN(v).is_valid, "pg_host is not a valid FQDN"
+        return v
 
     class Config:
         env_file = ".env"
